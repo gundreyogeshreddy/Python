3bae4ae1f81e4135fca59f958590178efe381ab2
Yogesh <136956748+gundreyogeshreddy@users.noreply.github.com>
2024-12-28 17:51:25 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,22 +1,22 @@
-# import boto3
+import boto3
 
-# # Initialize the S3 client
-# client = boto3.client('s3', region_name='ap-south-1') 
+# Initialize the S3 client
+client = boto3.client('s3', region_name='ap-south-1') 
 
-# # Specify the bucket name
-# bucket_name = 'newpb3bkt' 
+# Specify the bucket name
+bucket_name = 'newpb3bkt' 
 
-# # Delete the bucket
-# def delete_empty_bucket(bucket_name):
-#     try:
-#         # Delete the bucket
-#         client.delete_bucket(Bucket=bucket_name)
-#         print(f"Bucket '{bucket_name}' deleted successfully!")
-#     except Exception as e:
-#         print(f"Error deleting bucket: {e}")
+# Delete the bucket
+def delete_empty_bucket(bucket_name):
+    try:
+        # Delete the bucket
+        client.delete_bucket(Bucket=bucket_name)
+        print(f"Bucket '{bucket_name}' deleted successfully!")
+    except Exception as e:
+        print(f"Error deleting bucket: {e}")
 
-# # Call the function to delete the bucket
-# delete_empty_bucket(bucket_name)
+# Call the function to delete the bucket
+delete_empty_bucket(bucket_name)
 
 import boto3
 
